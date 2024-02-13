2f8cf3964be83e846b0a4cba28eeea82068178cb
Justin <justlee888@gmail.com>
2024-02-13 02:03:49 -0800

    test

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@
 import { Input } from "@/components/ui/input";
 import Login from "@/components/login";
 import { SessionProvider } from "next-auth/react";
-//test
 export default function Home() {
   return (
     <div className="flex h-full w-full justify-center">
