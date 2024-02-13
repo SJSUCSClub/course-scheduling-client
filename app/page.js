9c126e96369c8ad4df4829dfb16bdfa9fa6f7606
Justin <justlee888@gmail.com>
2024-02-12 23:29:55 -0800

    added shadcn

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 'use client'
+import { Input } from "@/components/ui/input"
 import GoogleButton from "react-google-button";
 export default function Home() {
   return (
     <div className="flex h-full w-full justify-center">
       <GoogleButton/>
+      {/* <Input/> */}
     </div>
   );
 }
