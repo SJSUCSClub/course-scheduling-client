359b253802a7a318dd110fcc49460a495bec6539
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-02-27 23:58:50 -0800

    Refactor rating-summary component styling

--- components/rating-summary.tsx
+++ components/rating-summary.tsx
@@ -20,18 +20,16 @@ export default function RatingSummary({
   ...props
 }: RatingSummaryProps) {
   return (
-    <div className="flex flex-1 items-end rounded-lg bg-ok p-[32px] opacity-75 default-border">
-      <p className="flex-1 text-caption text-text opacity-75 mix-blend-color-burn">
-        {reviewCount} Reviews
-      </p>
-      <div className="flex h-full flex-auto flex-col items-center justify-center gap-[3px] text-text opacity-75 mix-blend-color-burn">
+    <div className="flex flex-1 items-end rounded-lg bg-background p-[32px] default-border">
+      <p className="flex-1 text-caption text-text">{reviewCount} Reviews</p>
+      <div className="flex h-full flex-auto flex-col items-center justify-center gap-[3px] text-text">
         <h1 className="text-center text-title">{name}</h1>
         <h2 className="text-rating">{rating}</h2>
         <div className="h-[50px]">
           <Stars rating={rating} />
         </div>
       </div>
-      <div className="flex flex-1 justify-end gap-[5px] opacity-75 mix-blend-color-burn">
+      <div className="flex flex-1 justify-end gap-[5px]">
         <Button variant={<EnvelopeIcon />} className="text-text" />
         <Button variant={<ShareIcon />} className="text-text" />
         <Button variant={<BookmarkIcon />} className="text-text" />
