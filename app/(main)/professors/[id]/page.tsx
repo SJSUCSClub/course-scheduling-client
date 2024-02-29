1c27a1b916be5d2b4202a945385dd190e249028a
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-02-29 00:50:39 -0800

    Add charts for rating and grading distribution

--- app/(main)/professors/[id]/page.tsx
+++ app/(main)/professors/[id]/page.tsx
@@ -3,7 +3,6 @@ import {
   ArrowTopRightOnSquareIcon,
   ChevronRightIcon,
   ClipboardDocumentListIcon,
-  InformationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { notFound } from 'next/navigation';
 import React from 'react';
@@ -14,6 +13,9 @@ import Breadcrumb from '@/components/breadcrumb';
 import InfoCard from '@/components/info-card';
 import Button from '@/components/button';
 import Tag from '@/components/tag';
+import BarChart from '@/components/bar-chart';
+import LineChart from '@/components/line-chart';
+import Dropdown from '@/components/forms/dropdown';
 
 export async function generateMetadata({ params }: { params: { id: string } }) {
   return {
@@ -31,6 +33,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   const rating = 2.6;
   const name = 'Jahan Ghofraniha';
   const reviewCount = 47;
+  const email = 'YqVp3@example.com';
   const grade = 'A-';
   const wouldTakeAgain = '39%';
   const tags = [
@@ -47,7 +50,8 @@ export default async function Page({ params }: { params: { id: string } }) {
     'Lots of homework',
     'Tough grader',
   ];
-  const email = 'YqVp3@example.com';
+  const ratingDistribution = [11, 5, 1, 7, 23];
+  const gradeDistribution = [2, 15, 11, 20, 7];
 
   return (
     <main className="mx-auto flex max-w-[1076px] flex-col gap-[10px] p-[10px]">
@@ -97,6 +101,27 @@ export default async function Page({ params }: { params: { id: string } }) {
 
       {/* Statistics */}
       <SectionLabel info="Statistics">Statistics</SectionLabel>
+
+      <div className="flex gap-[10px] max-lg:flex-col">
+        <div className="flex min-h-[464px] flex-1 flex-col rounded-lg px-[32px] py-[20px] default-border">
+          <div className="flex justify-between">
+            <h3 className="flex h-[50px] items-center text-heading">
+              Rating Distribution
+            </h3>
+            <Dropdown
+              options={['Overall', 'Quality', 'Ease']}
+              values={['overall', 'quality', 'ease']}
+            />
+          </div>
+          <BarChart chartData={ratingDistribution} className="flex-1" />
+        </div>
+        <div className="flex min-h-[464px] flex-1 flex-col rounded-lg px-[32px] py-[20px] default-border">
+          <h3 className="flex h-[50px] items-center text-heading">
+            Grading Distribution
+          </h3>
+          <LineChart chartData={gradeDistribution} className="flex-1" />
+        </div>
+      </div>
 
       {/* Schedule */}
       <SectionLabel info="Statistics">Schedule</SectionLabel>
