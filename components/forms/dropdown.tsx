c89ae259fd7bbf2ef02698acdf26dbfeecdc50fd
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-02-29 00:50:24 -0800

    Add Dropdown component

---
+++ components/forms/dropdown.tsx
@@ -0,0 +1,64 @@
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+interface DropdownProps extends React.HTMLProps<HTMLDivElement> {
+  label?: string;
+  error?: string;
+  helper?: string;
+  required?: boolean;
+  disabled?: boolean;
+  options: string[];
+  values: string[];
+}
+
+export default function Dropdown({
+  label,
+  error,
+  helper,
+  required,
+  disabled,
+  options,
+  values,
+  ...props
+}: DropdownProps) {
+  return (
+    <div
+      {...props}
+      className={twMerge(
+        clsx('flex flex-col gap-[10px]', { 'opacity-30': disabled }),
+        props.className,
+      )}
+    >
+      <div className="flex items-center justify-between gap-[32px]">
+        {label ? (
+          <label className="flex gap-[3px] text-heading text-neutral">
+            {label}
+            {required ? <span className="text-important">*</span> : null}
+          </label>
+        ) : null}
+        {error ? (
+          <p className="flex text-subheading italic text-important">{error}</p>
+        ) : null}
+      </div>
+      <select
+        disabled={disabled}
+        className={twMerge(
+          'flex h-[40px] w-fit min-w-full appearance-none items-center rounded-md bg-border py-[5px] pl-[16px] pr-[38px] animation focus:border-primary focus:ring-0',
+          clsx({
+            'default-border': !error,
+            'border-2 border-important': error,
+          }),
+        )}
+      >
+        {options.map((option, index) => (
+          <option key={option} value={values[index]}>
+            {option}
+          </option>
+        ))}
+      </select>
+      {helper ? (
+        <p className="flex text-caption text-neutral">{helper}</p>
+      ) : null}
+    </div>
+  );
+}
