9ad90e469943e48dee23ae200940de9ad8b34a81
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-02-29 00:50:33 -0800

    Add BarChart and LineChart components

---
+++ components/bar-chart.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { Props } from 'react-apexcharts';
+import dynamic from 'next/dynamic';
+
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+const chartConfig: Props = {
+  type: 'bar',
+  height: 350,
+  options: {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+    },
+    title: {},
+    dataLabels: {
+      enabled: false,
+    },
+    colors: ['#b7d0ed'],
+    plotOptions: {
+      bar: {
+        columnWidth: '80%',
+        borderRadius: 6,
+      },
+    },
+    xaxis: {
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+      labels: {
+        style: {
+          colors: '#616161',
+          fontSize: '12px',
+          fontFamily: 'inherit',
+          fontWeight: 400,
+        },
+      },
+      categories: [5, 4, 3, 2, 1],
+    },
+    yaxis: {
+      labels: {
+        style: {
+          colors: '#616161',
+          fontSize: '12px',
+          fontFamily: 'inherit',
+          fontWeight: 400,
+        },
+      },
+    },
+    grid: {
+      show: true,
+      borderColor: '#dddddd',
+      strokeDashArray: 5,
+      xaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      padding: {
+        top: 5,
+        right: 20,
+      },
+    },
+    fill: { opacity: 0.8 },
+    tooltip: {
+      theme: 'light',
+    },
+  },
+};
+
+interface BarChartProps extends React.HTMLProps<HTMLDivElement> {
+  chartData: number[];
+}
+
+export default function BarChart({ chartData, ...props }: BarChartProps) {
+  return (
+    <div {...props}>
+      <Chart
+        series={[
+          {
+            name: 'Reviews',
+            data: chartData,
+          },
+        ]}
+        {...chartConfig}
+      />
+    </div>
+  );
+}
---
+++ components/line-chart.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { Props } from 'react-apexcharts';
+import dynamic from 'next/dynamic';
+
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+const chartConfig: Props = {
+  type: 'line',
+  height: 350,
+  options: {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+    },
+    title: {},
+    dataLabels: {
+      enabled: false,
+    },
+    colors: ['#b7d0ed'],
+    stroke: {
+      lineCap: 'round',
+      curve: 'smooth',
+    },
+    markers: {
+      size: 0,
+    },
+    xaxis: {
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+      labels: {
+        style: {
+          colors: '#616161',
+          fontSize: '12px',
+          fontFamily: 'inherit',
+          fontWeight: 400,
+        },
+      },
+      categories: ['F', 'D', 'C', 'B', 'A'],
+    },
+    yaxis: {
+      labels: {
+        style: {
+          colors: '#616161',
+          fontSize: '12px',
+          fontFamily: 'inherit',
+          fontWeight: 400,
+        },
+      },
+    },
+    grid: {
+      show: true,
+      borderColor: '#dddddd',
+      strokeDashArray: 5,
+      xaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      padding: {
+        top: 5,
+        right: 20,
+      },
+    },
+    fill: { opacity: 0.8 },
+    tooltip: {
+      theme: 'light',
+    },
+  },
+};
+interface LineChartProps extends React.HTMLProps<HTMLDivElement> {
+  chartData: number[];
+}
+
+export default function LineChart({ chartData, ...props }: LineChartProps) {
+  return (
+    <div {...props}>
+      <Chart
+        series={[
+          {
+            name: 'Students',
+            data: chartData,
+          },
+        ]}
+        {...chartConfig}
+      />
+    </div>
+  );
+}
