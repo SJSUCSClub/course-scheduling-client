1a73bb718fde2a2600449bf143a3f1c95ec3b650
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-03-02 19:10:42 -0800

    Rating summary style changes + added email prop

--- components/rating-summary.tsx
+++ components/rating-summary.tsx
@@ -6,6 +6,7 @@ import {
   ShareIcon,
 } from '@heroicons/react/24/outline';
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid';
+import { twMerge } from 'tailwind-merge';
 
 import { Popover } from '@/components/popover';
 import Button from '@/components/button';
@@ -15,6 +16,7 @@ interface RatingSummaryProps extends React.HTMLProps<HTMLDivElement> {
   reviewCount: number;
   name: string;
   rating: number;
+  email: string;
 }
 
 const Options: React.FC = () => {
@@ -31,12 +33,22 @@ const RatingSummary: React.FC<RatingSummaryProps> = ({
   reviewCount,
   name,
   rating,
+  email,
   ...props
 }) => {
   return (
-    <div className="flex flex-1 items-end rounded-lg bg-background p-[32px] default-border">
-      <p className="flex-1 text-caption text-text">{reviewCount} Reviews</p>
-      <div className="flex h-full flex-auto flex-col items-center justify-center gap-[3px] text-text">
+    <div
+      {...props}
+      className={twMerge(
+        'flex gap-[32px] rounded-lg bg-background p-[32px] text-text default-border lg:items-end',
+        props.className,
+      )}
+    >
+      <div className="flex flex-1 lg:hidden" />
+      <p className="min-w-[100px] flex-1 text-caption max-lg:hidden">
+        {reviewCount} Reviews
+      </p>
+      <div className="flex h-full flex-auto flex-col items-center justify-center gap-[3px]">
         <h1 className="text-center text-title">{name}</h1>
         <h2 className="text-rating">{rating}</h2>
         <div className="h-[50px]">
