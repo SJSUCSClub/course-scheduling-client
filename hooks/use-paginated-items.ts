a61514d21c310ce8b5d206050b3673dde7591139
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-03-12 18:12:28 -0700

    Fixed pagination showing the same content twice

--- hooks/use-paginated-items.ts
+++ hooks/use-paginated-items.ts
@@ -16,29 +16,27 @@ const usePaginatedItems = <
     currentPaginatedItems,
   );
 
-  const fetchPaginatedItems = React.useCallback(
+  const useFetchPaginatedItems = React.useCallback(
     (
-      (
-        page = initialFetchParams.params?.page
-          ? initialFetchParams.params.page + 1
-          : 0,
-      ) =>
+      page = initialFetchParams.params?.page !== undefined
+        ? initialFetchParams.params.page + 1
+        : 0,
+    ) =>
       async () => {
         const response = await fetchWithCache<Response, Params>({
           endpoint: initialFetchParams.endpoint,
           params: { ...(initialFetchParams.params as Params), page },
         });
         page = response?.page ?? page;
-        console.log(page);
         return response;
-      }
-    )(),
+      },
     [fetchWithCache, initialFetchParams],
   );
+  const fetchPaginatedItems = React.useRef(useFetchPaginatedItems());
 
   const loadMore = React.useCallback(async () => {
     if (isEndOfList) return;
-    const response = await fetchPaginatedItems();
+    const response = await fetchPaginatedItems.current();
     if (!response) return;
     if (response.items.length === 0) {
       setIsEndOfList(true);
@@ -48,7 +46,7 @@ const usePaginatedItems = <
       ...response,
       items: [...(paginatedItems?.items ?? []), ...response.items],
     });
-  }, [isEndOfList, fetchWithCache, paginatedItems]);
+  }, [isEndOfList, paginatedItems]);
   return { loading, error, isEndOfList, paginatedItems, loadMore };
 };
 
