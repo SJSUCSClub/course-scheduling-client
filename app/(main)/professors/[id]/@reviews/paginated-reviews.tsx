54c26bca5beaedee3b437c69139bcee852aa8c59
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-03-17 18:48:28 -0700

    removed granular filters for mobile to improve ux

--- app/(main)/professors/[id]/@reviews/paginated-reviews.tsx
+++ app/(main)/professors/[id]/@reviews/paginated-reviews.tsx
@@ -6,13 +6,13 @@ import {
   ProfessorReviewsRouteParams,
   ProfessorReviewsRouteResponse,
 } from '@/utils/types';
-import { TagCheckbox, TagCheckboxGroup } from '@/components/forms/tag-checkbox';
+import Filters from '@/app/(main)/professors/[id]/@reviews/filters';
+import usePaginatedItems from '@/hooks/use-paginated-items';
 import SectionLabel from '@/components/section-label';
 import { FetchParams } from '@/utils/fake-fetch';
-import usePaginatedItems from '@/hooks/use-paginated-items';
+import Dropdown from '@/components/forms/dropdown';
 import Review from '@/components/review';
 import Button from '@/components/button';
-import Dropdown from '@/components/forms/dropdown';
 
 const PaginatedReviews: React.FC<{
   initialPaginatedReviews: ProfessorReviewsRouteResponse;
@@ -51,35 +51,13 @@ const PaginatedReviews: React.FC<{
 
   return (
     <main className="flex items-stretch gap-[10px]">
-      <div className="w-[250px]">
+      <div className="w-[250px] max-lg:hidden">
         <div className="sticky top-0 flex h-[100dvh] w-full flex-col gap-[10px] overflow-y-auto">
-          <SectionLabel>Filters</SectionLabel>
-          <TagCheckboxGroup
-            onChange={handleSetFilters('tags')}
-            label="Tags"
-            disabled={loading}
-          >
-            {paginatedItems?.filters.tags.map((tag) => (
-              <TagCheckbox key={tag.tag} value={tag.tag} count={tag.count}>
-                {tag.tag}
-              </TagCheckbox>
-            ))}
-          </TagCheckboxGroup>
-          <TagCheckboxGroup
-            onChange={handleSetFilters('courses')}
-            label="Courses"
-            disabled={loading}
-          >
-            {paginatedItems?.filters.courses.map((tag) => (
-              <TagCheckbox
-                key={tag.course}
-                value={tag.course}
-                count={tag.count}
-              >
-                {tag.course}
-              </TagCheckbox>
-            ))}
-          </TagCheckboxGroup>
+          <Filters
+            handleSetFilters={handleSetFilters}
+            loading={loading}
+            paginatedItems={paginatedItems}
+          />
         </div>
       </div>
       <div className="flex flex-1 flex-col gap-[10px] pb-[10px]">
