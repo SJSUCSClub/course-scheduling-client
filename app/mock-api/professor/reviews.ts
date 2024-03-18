a6b29f1b648ce54405743aaedd3ac35826b3b334
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-03-17 18:08:28 -0700

    updated reviews endpoint to return filters

--- app/mock-api/professor/reviews.ts
+++ app/mock-api/professor/reviews.ts
@@ -6,27 +6,13 @@ import {
 import getPaginatedItems from '@/utils/get-paginated-items';
 import chooseRandom from '@/utils/choose-random';
 
-const tags: TagType[] = [
-  'Respected',
-  'Caring',
-  'Amazing lectures',
-  'Inspirational',
-  'Accessible outside class',
-  'Participation matters',
-  'Graded by few things',
-  'Clear grading criteria',
-  'Get ready to read',
-  'Lots of homework',
-  'Tough grader',
-];
-
 type ProfessorReview = ProfessorReviewsRouteResponse['items'][number];
 
 const reviews: ProfessorReview[] = Array.from<undefined, ProfessorReview>(
   { length: 100 },
   (v, k): ProfessorReview => {
-    const quality = Math.max(0.5, Math.ceil(Math.random() * 50) / 10);
-    const ease = Math.max(0.5, Math.ceil(Math.random() * 50) / 10);
+    const quality = (k % 5) + 1;
+    const ease = (k % 10) / 2 + 1;
     return {
       id: k,
       createdAt: 'August 19, 2002',
@@ -37,18 +23,18 @@ const reviews: ProfessorReview[] = Array.from<undefined, ProfessorReview>(
       quality,
       ease,
       overall: Math.max(0.5, Math.ceil(((quality + ease) / 2) * 10) / 10),
-      grade: chooseRandom(['C-', 'A+', 'F']),
-      takeAgain: chooseRandom([true, false]),
-      tags: Array.from({ length: Math.ceil(Math.random() * 8) }, () =>
-        chooseRandom(tags),
-      ),
+      grade: k % 3 === 0 ? 'A+' : k % 2 === 0 ? 'C-' : 'F',
+      takeAgain: k % 2 === 0,
+      tags:
+        k % 5 === 0
+          ? ['Easy grader', 'Funny']
+          : ['Easy grader', 'Tough grader'],
       courseId: 'CMPE132',
-      isUserAnonymous: k % 2 === 0,
+      isUserAnonymous: k % 5 === 0,
       userId: 1,
-      professorId: 2,
       upvotes: k,
       userName: 'John Doe',
-      professorName: 'Jahan Ghofraniha',
+      professorId: k % 2 === 0 ? 2 : 3,
     };
   },
 );
@@ -57,17 +43,16 @@ export const response = ({
   id,
   itemsPerPage,
   page,
-  sort,
   filters,
 }: ProfessorReviewsRouteParams): ProfessorReviewsRouteResponse => {
   const result = reviews
     .filter((review) => {
-      if (filters?.tags) {
+      if (filters?.tags?.length) {
         if (!filters.tags.every((tag) => review.tags.includes(tag))) {
           return false;
         }
       }
-      if (filters?.courses) {
+      if (filters?.courses?.length) {
         if (!filters.courses.includes(review.courseId)) {
           return false;
         }
@@ -78,20 +63,43 @@ export const response = ({
       return true;
     })
     .sort((a, b) => {
-      if (sort === 'newest') {
+      if (filters?.sort === 'newest') {
         return (
           new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
         );
-      } else if (sort === 'highest') {
-        return b?.overall && a?.overall ? a.overall - b.overall : -1;
-      } else if (sort === 'lowest') {
-        return b?.overall && a?.overall ? b.overall - a.overall : -1;
+      } else if (filters?.sort === 'highest') {
+        return b.overall - a.overall;
+      } else if (filters?.sort === 'lowest') {
+        return a.overall - b.overall;
       } else {
         return b.upvotes - a.upvotes;
       }
     });
+  const tagFilters = new Map<TagType, number>();
+  result.forEach((review) => {
+    review.tags.forEach((tag) => {
+      const currentCount = tagFilters.get(tag) ?? 0;
+      tagFilters.set(tag, currentCount + 1);
+    });
+  });
+  const courseFilters = new Map<string, number>();
+  result.forEach((review) => {
+    const currentCount = courseFilters.get(review.courseId) ?? 0;
+    courseFilters.set(review.courseId, currentCount + 1);
+  });
   return {
-    totalReviews: reviews.length,
+    totalReviews: result.length,
+    filters: {
+      sort: filters?.sort ?? 'relevant',
+      tags: Array.from(tagFilters.entries()).map(([tag, count]) => ({
+        tag,
+        count,
+      })),
+      courses: Array.from(courseFilters.entries()).map(([course, count]) => ({
+        course,
+        count,
+      })),
+    },
     ...getPaginatedItems<ProfessorReview>({
       items: result,
       itemsPerPage,
