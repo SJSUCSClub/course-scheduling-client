0c090a939dadd6d9b0d995173b46a6b10a9bf5af
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-03-18 21:22:33 -0700

    reorganize types

---
+++ types/general.ts
@@ -0,0 +1,69 @@
+import { Review, Schedule, User } from '@/types/database';
+
+/***********************************************
+ * General types. These are reused often and are not specific to any part of the app.
+ ***********************************************/
+export type GradeType =
+  | 'A+'
+  | 'A'
+  | 'A-'
+  | 'B+'
+  | 'B'
+  | 'B-'
+  | 'C+'
+  | 'C'
+  | 'C-'
+  | 'D+'
+  | 'D'
+  | 'D-'
+  | 'F';
+export type TagType =
+  | 'Easy grader'
+  | 'Lots of assignments'
+  | 'Tough grader'
+  | 'Funny';
+export type RatingType = number;
+export type PercentageType = number;
+export type AvailabilityType = number;
+export type EvaluationType = 'bad' | 'ok' | 'good';
+export type DistributionType = [number, number, number, number, number];
+export type WeekdayType = 'M' | 'T' | 'W' | 'R' | 'F';
+export type SortType = 'relevant' | 'newest' | 'oldest' | 'highest' | 'lowest';
+export type DateType = string;
+export type PaginatedItems<T> = {
+  items: T[];
+  itemsPerPage?: number;
+  page: number;
+};
+export type GenericScheduleType = Pick<
+  Schedule,
+  | 'professorId'
+  | 'courseId'
+  | 'classNumber'
+  | 'section'
+  | 'dates'
+  | 'times'
+  | 'availableSeats'
+  | 'location'
+  | 'modeOfInstruction'
+> &
+  Pick<Review, 'grade' | 'overall'> & {
+    days?: WeekdayType[];
+  };
+export type GenericReviewType = Pick<
+  Review,
+  | 'id'
+  | 'createdAt'
+  | 'content'
+  | 'quality'
+  | 'ease'
+  | 'overall'
+  | 'grade'
+  | 'takeAgain'
+  | 'tags'
+  | 'isUserAnonymous'
+  | 'userId'
+> & {
+  upvotes: number;
+  userName: User['name'];
+};
