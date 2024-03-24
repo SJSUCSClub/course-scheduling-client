b80f514ed0b4259346ed5c7a84afa4743a2895ae
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-03-24 11:44:56 -0700

    added a professor

--- app/mock-api/professor/summary.ts
+++ app/mock-api/professor/summary.ts
@@ -21,6 +21,22 @@ const professors: ProfessorSummaryRouteResponse[] = [
     takeAgain: 52,
     tags: ['Easy grader', 'Funny', 'Lots of assignments', 'Tough grader'],
   },
+  {
+    id: 3,
+    name: 'Kurt Mammen',
+    email: 'test@example.com',
+    quality: 1.6,
+    ease: 5,
+    overall: 5,
+    grade: 'B-',
+    overallDistribution: [11, 5, 1, 7, 23],
+    qualityDistribution: [4, 3, 2, 9, 29],
+    easeDistribution: [2, 15, 11, 20, 9],
+    gradeDistribution: [2, 15, 11, 20, 9],
+    totalReviews: 60,
+    takeAgain: 90,
+    tags: ['Easy grader', 'Lots of assignments', 'Tough grader'],
+  },
 ];
 
 export const response: FakeResponseFunctionType<
