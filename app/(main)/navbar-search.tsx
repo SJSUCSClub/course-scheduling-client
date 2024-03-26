02d24744dab099f712f96c2d8416a5a59bec20e6
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-03-25 22:15:44 -0700

    made navbar search work

--- app/(main)/navbar-search.tsx
+++ app/(main)/navbar-search.tsx
@@ -1,11 +1,28 @@
+'use client';
+
 import clsx from 'clsx';
+import React from 'react';
 
 import Icon from '@/components/icon';
+import LoadingSpinner from '@/components/loading-spinner';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
-const NavbarSearch: React.FC<React.HTMLProps<HTMLDivElement>> = (props) => {
+const NavbarSearch: React.FC<React.HTMLProps<HTMLFormElement>> = (props) => {
+  const [option, setOption] = React.useState('courses');
+  const [loading, setLoading] = React.useState(false);
+  const [search, setSearch] = React.useState('');
+  const handleSetOption = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setOption(event.target.value);
+  };
+  const handleSetSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+  React.useEffect(() => {
+    setLoading(false);
+  }, [search]);
   return (
-    <div
+    <form
+      action={`/${option}/search?query=${search}`}
       {...props}
       className={clsx(
         `-:flex -:h-[40px] -:text-body -:text-text`,
@@ -13,20 +30,29 @@ const NavbarSearch: React.FC<React.HTMLProps<HTMLDivElement>> = (props) => {
       )}
     >
       <input
+        onChange={handleSetSearch}
         type="text"
         className="h-full w-0 flex-1 rounded-l-md bg-border pr-10 animation default-border focus:border-primary focus:ring-0"
         placeholder="Search"
       />
       <div className="relative h-full">
         <div className="absolute right-4 flex h-full w-[16px] items-center">
-          <Icon icon={<MagnifyingGlassIcon />} w="16px" h="16px" />
+          <Icon
+            icon={loading ? <LoadingSpinner /> : <MagnifyingGlassIcon />}
+            w="16px"
+            h="16px"
+          />
         </div>
       </div>
-      <select className="flex h-full w-fit appearance-none items-center rounded-r-md bg-background py-[5px] pl-[16px] pr-[38px] animation default-border focus:border-primary focus:ring-0">
+      <select
+        onChange={handleSetOption}
+        defaultValue={option}
+        className="flex h-full w-fit appearance-none items-center rounded-r-md bg-background py-[5px] pl-[16px] pr-[38px] animation default-border focus:border-primary focus:ring-0"
+      >
         <option value="courses">Courses</option>
         <option value="professors">Professors</option>
       </select>
-    </div>
+    </form>
   );
 };
 
