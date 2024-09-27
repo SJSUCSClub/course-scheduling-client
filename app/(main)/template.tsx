d065a07a42468d137a3764901cdd08d6a7e0fa60
ahmadgaz <gazali.contact@gmail.com>
2024-09-26 17:36:52 -0700

    switched layout with navbar to template

--- app/(main)/template.tsx
+++ app/(main)/template.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from '@/components/organisms';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
