3b0871c39f581b8fcbf485f6ea457ea7918351f3
ahmadgaz <gazali.contact@gmail.com>
2024-10-10 18:13:42 -0700

    added no data state

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -32,8 +32,9 @@ export default async function Page({
           </ParamSelect>
         </div>
         <div className="flex flex-1 flex-col items-stretch gap-md pb-lg">
-          {searchParams.view === 'votes'
-            ? data.reviews_voted.map((item, i) => (
+          {searchParams.view === 'votes' ? (
+            data.reviews_voted.length > 0 ? (
+              data.reviews_voted.map((item, i) => (
                 <Review
                   key={i}
                   link={`/professors/${item.professor_id}`}
@@ -59,32 +60,43 @@ export default async function Page({
                   courseId={`${item.department}-${item.course_number}`}
                 />
               ))
-            : data.reviews.map((item, i) => (
-                <Review
-                  key={i}
-                  link={`/professors/${item.professor_id}`}
-                  title={item.professor_name}
-                  name={
-                    item.reviewer_name ??
-                    item.reviewer_username ??
-                    'Anonymous User'
-                  }
-                  createdAt={item.created_at}
-                  updatedAt={item.updated_at}
-                  content={item.content}
-                  quality={item.quality}
-                  ease={item.ease}
-                  grade={item.grade}
-                  tags={item.tags}
-                  takeAgain={item.take_again}
-                  votes={item.votes}
-                  userId={item.user_id}
-                  id={item.id.toString()}
-                  userVote={item.user_vote}
-                  professorId={item.professor_id}
-                  courseId={`${item.department}-${item.course_number}`}
-                />
-              ))}
+            ) : (
+              <p className="w-full p-md text-center italic text-neutral">
+                You haven&apos;t written any reviews.
+              </p>
+            )
+          ) : data.reviews.length > 0 ? (
+            data.reviews.map((item, i) => (
+              <Review
+                key={i}
+                link={`/professors/${item.professor_id}`}
+                title={item.professor_name}
+                name={
+                  item.reviewer_name ??
+                  item.reviewer_username ??
+                  'Anonymous User'
+                }
+                createdAt={item.created_at}
+                updatedAt={item.updated_at}
+                content={item.content}
+                quality={item.quality}
+                ease={item.ease}
+                grade={item.grade}
+                tags={item.tags}
+                takeAgain={item.take_again}
+                votes={item.votes}
+                userId={item.user_id}
+                id={item.id.toString()}
+                userVote={item.user_vote}
+                professorId={item.professor_id}
+                courseId={`${item.department}-${item.course_number}`}
+              />
+            ))
+          ) : (
+            <p className="w-full p-md text-center italic text-neutral">
+              You haven&apos;t interacted with any reviews.
+            </p>
+          )}
         </div>
       </section>
     </main>
